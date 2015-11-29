533265f748e00c3d7ad331b97337e903214951cd
Chris Lundie <chris@lundie.ca>
2015-11-29 18:21:53 -0500

    Enable login item at app launch if preference is set

--- Ejector/EJELoginItemEnabler.m
+++ Ejector/EJELoginItemEnabler.m
@@ -19,18 +19,27 @@
 
 @end
 
+static void setLoginItemEnabled(BOOL isEnabled);
+static BOOL shouldEnableLoginItem(NSUserDefaultsController *controller);
+
+static void setLoginItemEnabled(BOOL isEnabled) {
+  SMLoginItemSetEnabled(CFSTR("ca.lundie.EjectorLoginItem"), isEnabled ? true : false);
+}
+
+static BOOL shouldEnableLoginItem(NSUserDefaultsController *controller) {
+  id value = [controller valueForKeyPath:@"values.LoginItemEnabled"];
+  return [value isKindOfClass:[NSNumber class]] ? [value boolValue] : NO;
+}
+
 @implementation EJELoginItemEnabler
 
 - (instancetype)watchUserDefaults
 {
   NSUserDefaultsController *controller = [NSUserDefaultsController eje_sharedSuite];
+  setLoginItemEnabled(shouldEnableLoginItem(controller));
   self.observers = @[
     [controller cdl_observeKeyPaths:@[@"values.LoginItemEnabled",] options:0 block:^(NSString * _Nullable _, NSDictionary<NSString *,id> * _Nullable change) {
-      id value = [controller valueForKeyPath:@"values.LoginItemEnabled"];
-      NSLog(@"%@", value);
-      BOOL loginItemEnabled = [value isKindOfClass:[NSNumber class]] ? [value boolValue] : NO;
-      [[controller defaults] synchronize];
-      SMLoginItemSetEnabled(CFSTR("ca.lundie.EjectorLoginItem"), loginItemEnabled ? true : false);
+      setLoginItemEnabled(shouldEnableLoginItem(controller));
     }],
 
     [controller cdl_observeKeyPaths:@[@"values.Foo", @"values.LoginItemEnabled",] options:0 block:^(NSString * _Nullable _, NSDictionary<NSString *,id> * _Nullable change) {
