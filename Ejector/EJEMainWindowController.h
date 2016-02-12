3d78c9408f682d2b1bc2a91a3f3ae9854378ff38
Chris Lundie <chris@lundie.ca>
2016-02-11 19:21:10 -0500

    Autosave main window

---
+++ Ejector/EJEMainWindowController.h
@@ -0,0 +1,13 @@
+//
+//  EJEMainWindowController.h
+//  Ejector
+//
+//  Created by Chris Lundie on 11/Feb/2016.
+//  Copyright © 2016 Chris Lundie. All rights reserved.
+//
+
+@import AppKit.NSWindowController;
+
+@interface EJEMainWindowController : NSWindowController
+
+@end
---
+++ Ejector/EJEMainWindowController.m
@@ -0,0 +1,17 @@
+//
+//  EJEMainWindowController.m
+//  Ejector
+//
+//  Created by Chris Lundie on 11/Feb/2016.
+//  Copyright © 2016 Chris Lundie. All rights reserved.
+//
+
+#import "EJEMainWindowController.h"
+
+@implementation EJEMainWindowController
+
+- (void)awakeFromNib {
+  self.windowFrameAutosaveName = @"EjectorMainWindow";
+}
+
+@end
